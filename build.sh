0d9e3c1dc59947f52fe0212755ee20aca0007a32
TeeTinnapop <tinnapop.d@securitypitch.com>
2025-12-05 14:19:46 +0700

    fix: update cors and version

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+VERSION="v1.0"
+
+docker login
+
+# Build the backend
+echo "Building backend..."
+docker build --platform linux/amd64 -t google-ai-backend:latest ./backend
+docker tag google-ai-backend:latest teetinnapop/cicd:google-ai-backend-$VERSION
+docker push teetinnapop/cicd:google-ai-backend-$VERSION
+
+# Build the frontend
+echo "Building frontend..."
+docker build --platform linux/amd64 -t google-ai-frontend:latest ./frontend
+docker tag google-ai-frontend:latest teetinnapop/cicd:google-ai-frontend-$VERSION
+docker push teetinnapop/cicd:google-ai-frontend-$VERSION
